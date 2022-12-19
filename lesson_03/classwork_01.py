11a81543a7f85751599ef9b5a6782875368d875b
pervyshev1 <kingtiger27062004@gmail.com>
2022-12-19 21:58:30 +0300

    Classwork_01

---
+++ lesson_03/classwork_01.py
@@ -0,0 +1,26 @@
+#Задание 1
+print("   Задание 1")
+x=17/2*3 + 2
+print(x)
+x = 2 + 17 / 2 * 3
+print(x)
+x = 19 % 4 + 15 / 2 * 3
+print(x)
+x = (15 + 6) - 10 * 4
+print(x)
+x = 17 / 2 % 2 * 3
+print(x)
+#Задание 2
+print("   Задание 2")
+x = (15 + 6 - 10) * 4
+print(x)
+x = 19 % (4 + 15) / 2 * 3
+print(x)
+#Задание 3,4 (Легкое)
+#Задание 5
+print("   Задание 5")
+my_list = [1, 1.0, 2, 2, 5.0, "python", "python3", "python3"]
+my_set = set(my_list)
+print(len(my_list)-len(my_set))
+
+
