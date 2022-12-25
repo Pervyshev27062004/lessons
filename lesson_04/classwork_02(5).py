fcfec3943f915a1713d6b727aa37c0f0f99faebe
pervyshev1 <kingtiger27062004@gmail.com>
2022-12-25 21:45:54 +0300

    Classwork_01

--- lesson_04/classwork_02(5).py
+++ lesson_04/classwork_02(5).py
@@ -1,6 +1,8 @@
 n = int(input())
 m = int(input())
 result = 0
-for i in range(n,m):
-    n+=1
-    result =
+for i in range(n,m,1):
+    count = i ** 3
+    print(count)
+    result += count
+print(result)
