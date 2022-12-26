a4bb97a1af483bf2ea185d14fafca30d3fd1f5c8
pervyshev1 <kingtiger27062004@gmail.com>
2022-12-26 21:07:12 +0300

    Classwork_02

---
+++ lesson_03/classwork_01.py
@@ -0,0 +1,38 @@
+#Задание 1
+print("   Задание 1")
+x=17/2*3 + 2
+print(x)
+x = 2 + 17 / 2 * 3
+print(x)
+x = 19 % 4 + 15 / 2 * 3
+print(x)
+x = (15 + 6) - 10 * 4
+print(x)
+x = 17 / 2 % 2 * 3
+print(x)
+#Задание 2
+print("   Задание 2")
+x = (15 + 6 - 10) * 4
+print(x)
+x = 19 % (4 + 15) / 2 * 3
+print(x)
+#Задание 3,4 (Легкое)
+#Задание 5
+print("   Задание 5")
+my_list = [1, 1.0, 2, 2, 5.0, "python", "python3", "python3"]
+my_set = set(my_list)
+print(len(my_list)-len(my_set))
+#Задание 6
+print("   Задание 6")
+my_list = [1, 1.0, 2, 2, 5.0, "python", "python3", "python3"]
+my_list[3], my_list[2], my_list[1]
+print(my_list[3])
+print(my_list[2])
+print(my_list[1])
+#Задание 7
+print("   Задание 7")
+a=int(input())
+print(a*4,"периметр")
+print(a*a,"площадь")
+print(a*1.414,"диагональ")
+
---
+++ lesson_03/solution_02.py
@@ -0,0 +1,3 @@
+#ВТОРОЕ ЗАДАНИЕ
+my_list=["B","e","r","l","i","n"]
+print(my_list[-1],my_list[-2],my_list[-3],my_list[-4],my_list[-5],my_list[-6])
