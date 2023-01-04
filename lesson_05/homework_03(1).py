36d7b3ec96ff5e6a502d9356d52dc97761066943
pervyshev1 <kingtiger27062004@gmail.com>
2023-01-04 19:36:35 +0300

    Classwork_03

--- lesson_05/homework_03(1).py
+++ lesson_05/homework_03(1).py
@@ -1,15 +1,4 @@
 def three_args(var1, var2, var3):
-    my_dict = locals()
-    print("Переданные данные:", *(f"{var} = {number}" for var, number in my_dict.items() if number))
-
-
-
-three_args(1, None, 10)
-"""
-Для решения задачи используйте функцию locals (), которая в виде словаря представит все внутренние аргументы функции. 
-"""
-
-def three_args(var1, var2, var3):
     print(f"Переданные данные: var1 = {var1} var2 = {var2} var3 = {var3}")
 
 
--- lesson_05/homework_03(2).py
+++ lesson_05/homework_03(2).py
@@ -1,17 +1,12 @@
 import random
-from random import randint
-def santa(var1, var2, var3, var4, var7, var8):
-    var5 = 0
-    var6 = 0
-    x = random.randint(1,10)
-    print(x)
-    if x <= 3:
-        var5 = var2
-        
-    if x > 3 and x <= 6:
-        var5 = var3
-    if x > 6:
-        var5 = var4
 
-    print(f"1 пара: {var1} - {var5} 2 пара: {var3} - {var6} 3 пара: {}")
-santa("Alex","Sasha","Masha","Adolf","Josef", "Jesus")
+people = ["ALEX", "sasha", "masha", "maks"]
+people = ["ALEX", "sasha", "masha", "maks"]
+
+random.shuffle(people)
+
+for index in range(len(people)):
+    if index == len(people) - 1:
+        print(f"{people[index]} - {people[0]}")
+    else:
+        print(f"{people[index]} - {people[index + 1]}")
